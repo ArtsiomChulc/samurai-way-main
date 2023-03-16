96dd84042b2b15859234bc11a2967f4cc9c4cf61
Artsiom Chults <artemorsha@mail.ru>
2023-03-17 00:58:27 +0300

    styled friends component

--- src/component/Navbar/Friends/Friends.tsx
+++ src/component/Navbar/Friends/Friends.tsx
@@ -6,14 +6,18 @@ export const Friends = (props: NavbarFriendsType) => {
 
 	return (
 		<div className={s.friendsWrap}>
-			{props.friends.map(el => {
-				return (
-					<div key={el.id} className={s.friend}>
-						<img src={el.imageUrl} alt="Avatar" />
-						<p>{el.name}</p>
-					</div>
-				)
-			})}
+			<h3>Friends:</h3>
+			<div className={s.friendFlex}>
+				{props.friends.map(el => {
+					return (
+						<div key={el.id} className={s.friend}>
+							<img src={el.imageUrl} alt="Avatar" />
+							<p>{el.name}</p>
+						</div>
+					)
+				})}
+			</div>
+
 		</div>
 	)
 } 
