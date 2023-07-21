c4ee282e33fea764cf24008a5396bc938aa065a3
ArtsiomChulc <artemorsha@mail.ru>
2023-07-21 16:37:08 +0300

    lesson 93 add material UI (userCard)

--- src/component/Navbar/Friends/Friends.tsx
+++ src/component/Navbar/Friends/Friends.tsx
@@ -2,19 +2,19 @@ import s from "./friends.module.css";
 import { FriendsPropsType } from "./FriendsContainer";
 
 export const Friends = (props: FriendsPropsType) => {
-  return (
-    <div className={s.friendsWrap}>
-      <h3>Friends:</h3>
-      <div className={s.friendFlex}>
-        {props.friends.map((el) => {
-          return (
-            <div onClick={props.v} key={el.id} className={s.friend}>
-              <img src={el.imageUrl} alt="Avatar" />
-              <p>{el.name}</p>
+    return (
+        <div className={s.friendsWrap}>
+            <h3>Friends:</h3>
+            <div className={s.friendFlex}>
+                {props.friends.map((el) => {
+                    return (
+                        <div onClick={props.v} key={el.id} className={s.friend}>
+                            <img src={el.imageUrl} alt="Avatar" />
+                            <p>{el.name}</p>
+                        </div>
+                    );
+                })}
             </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
--- src/component/Users/User.tsx
+++ src/component/Users/User.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import { UsersType } from "Redux/users-reducer";
 import { NavLink } from "react-router-dom";
 import DotPreloader from "../common/dotPreloader/DotPreloader";
+import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
 
 type UserPropsType = {
     user: UsersType;
@@ -17,45 +18,88 @@ export const User = (props: UserPropsType) => {
     const { user, followingProgress, follow, unFollow } = props;
 
     return (
-        <div key={user.id} className={s.userWrap}>
-            <div className={s.userPhoto}>
-                <NavLink to={"/Profile/" + user.id}>
-                    <img src={user.photos.small != null ? user.photos.small : userPhoto} alt="User photo" />
-                </NavLink>
-            </div>
-            <div className={s.titleInfo}>
-                <div className={s.info}>
-                    <p>{user.name}</p>
-                    <p>{user.status}</p>
-                </div>
-                <div className={s.location}>
-                    <p>{"user.location.country"}</p>
-                    <p>{"user.location.city"}</p>
-                </div>
-            </div>
-            <div className={s.btns}>
-                {user.followed ? (
-                    <button
-                        disabled={followingProgress.some((id) => id === user.id)}
-                        className={`${btn.btn} ${s.btn}`}
-                        onClick={() => {
-                            unFollow(user.id);
-                        }}
-                    >
-                        {followingProgress.some((id) => id === user.id) ? <DotPreloader /> : "Unfollow"}
-                    </button>
-                ) : (
-                    <button
-                        disabled={followingProgress.some((id) => id === user.id)}
-                        className={`${btn.btn} ${s.btn}`}
-                        onClick={() => {
-                            follow(user.id);
-                        }}
-                    >
-                        {followingProgress.some((id) => id === user.id) ? <DotPreloader /> : "Follow"}
-                    </button>
-                )}
-            </div>
+        // <div key={user.id} className={s.userWrap}>
+        <div>
+            {/*<div className={s.userPhoto}>*/}
+            {/*    <NavLink to={"/Profile/" + user.id}>*/}
+            {/*        <img src={user.photos.small != null ? user.photos.small : userPhoto} alt="User photo" />*/}
+            {/*    </NavLink>*/}
+            {/*</div>*/}
+            {/*<div className={s.titleInfo}>*/}
+            {/*    <div className={s.info}>*/}
+            {/*        <p>{user.name}</p>*/}
+            {/*        <p>{user.status}</p>*/}
+            {/*    </div>*/}
+            {/*    <div className={s.location}>*/}
+            {/*        <p>{"user.location.country"}</p>*/}
+            {/*        <p>{"user.location.city"}</p>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+            {/*<div className={s.btns}>*/}
+            {/*    {user.followed ? (*/}
+            {/*        <button*/}
+            {/*            disabled={followingProgress.some((id) => id === user.id)}*/}
+            {/*            className={`${btn.btn} ${s.btn}`}*/}
+            {/*            onClick={() => {*/}
+            {/*                unFollow(user.id);*/}
+            {/*            }}*/}
+            {/*        >*/}
+            {/*            {followingProgress.some((id) => id === user.id) ? <DotPreloader /> : "Unfollow"}*/}
+            {/*        </button>*/}
+            {/*    ) : (*/}
+            {/*        <button*/}
+            {/*            disabled={followingProgress.some((id) => id === user.id)}*/}
+            {/*            className={`${btn.btn} ${s.btn}`}*/}
+            {/*            onClick={() => {*/}
+            {/*                follow(user.id);*/}
+            {/*            }}*/}
+            {/*        >*/}
+            {/*            {followingProgress.some((id) => id === user.id) ? <DotPreloader /> : "Follow"}*/}
+            {/*        </button>*/}
+            {/*    )}*/}
+            {/*</div>*/}
+            <Card sx={{ maxWidth: 245 }}>
+                <CardMedia
+                    sx={{ height: 180 }}
+                    image={user.photos.small != null ? user.photos.small : userPhoto}
+                    title="users"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {user.name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all
+                        continents except Antarctica
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    {user.followed ? (
+                        <Button
+                            size="small"
+                            disabled={followingProgress.some((id) => id === user.id)}
+                            onClick={() => {
+                                unFollow(user.id);
+                            }}
+                        >
+                            {followingProgress.some((id) => id === user.id) ? <DotPreloader /> : "Unfollow"}
+                        </Button>
+                    ) : (
+                        <Button
+                            size="small"
+                            disabled={followingProgress.some((id) => id === user.id)}
+                            onClick={() => {
+                                follow(user.id);
+                            }}
+                        >
+                            {followingProgress.some((id) => id === user.id) ? <DotPreloader /> : "Follow"}
+                        </Button>
+                    )}
+
+                    {/*<Button size="small">Share</Button>*/}
+                    {/*<Button size="small">Learn More</Button>*/}
+                </CardActions>
+            </Card>
         </div>
     );
 };
