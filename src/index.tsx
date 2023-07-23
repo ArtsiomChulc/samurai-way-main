e36963c6ce73f80ab95e57008c389cefa6a875f7
ArtsiomChulc <artemorsha@mail.ru>
2023-07-23 17:55:07 +0300

    lesson 95 HashRouter (basename)

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,15 @@
 import store from "./Redux/redux-store";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import React from "react";
 import { Provider } from "react-redux";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App
-      // store={store}
-      // addPost={addPost}
-      // updateNewPostText={updateNewPostText}
-      // addMessage={store.addMessage}
-      // updateNewMessageText={store.updateNewMessageText}
-      />
-    </Provider>
-  </BrowserRouter>,
-  document.getElementById("root"),
+    <HashRouter basename={process.env.PUBLIC_URL}>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </HashRouter>,
+    document.getElementById("root"),
 );
